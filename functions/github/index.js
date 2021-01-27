7798b3a0d4daa777b73659df2be8f0ba71ba343a
Didier Catz <didierlennon@gmail.com>
2021-01-27 16:21:08 +0100

    Added .DS_Store _redirects functions/github/index.js index.html index.js netlify.toml.

---
+++ functions/github/index.js
@@ -0,0 +1,7 @@
+// exports.handler = async function (event, context) {
+//   console.log({ event });
+//   const { queryStringParameters } = event;
+//   const { repo } = queryStringParameters;
+
+//   return { statusCode: 200, body: JSON.stringify({ repo }) };
+// };
