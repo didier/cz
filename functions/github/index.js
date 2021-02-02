54b1b3ebe7970fb669fc23b77c2fb720cb57d6a7
didier <didierlennon@gmail.com>
2021-02-02 16:57:46 +0100

    Added .cache/4c/dfae03bca357733eb9358972ad03e3.json .cache/83/5bb739b4352b731b7aa3edc5d1498b.json .cache/91/9bd711148ec66fc350a8afed32f687.json .cache/c7/80be1d6f46f312fd9a3c80d30bff75.json dist/_redirects dist/back.f430ff5f.svg dist/cz.e31bb0bc.js dist/cz.e31bb0bc.js.map dist/front.ff5aea96.svg dist/index.js dist/index.js.map dist/meta.ce82f70c.png dist/qr.abcb3809.svg functions/github/index.js package.json.

--- functions/github/index.js
+++
@@ -1,7 +0,0 @@
-// exports.handler = async function (event, context) {
-//   console.log({ event });
-//   const { queryStringParameters } = event;
-//   const { repo } = queryStringParameters;
-
-//   return { statusCode: 200, body: JSON.stringify({ repo }) };
-// };
